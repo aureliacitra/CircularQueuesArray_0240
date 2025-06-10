9e66b25958c2fff8db8290053aba1a5a66dbb510
aureliacitra <citrapangukir@gmail.com>
2025-06-10 10:57:24 +0700

    add void remove

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -37,5 +37,26 @@ class Queues {
     queue_array[REAR] = num;
 
     }
+ 
+    void remove() {
+        if (FRONT == -1) {
+            cout << "Queue underflow\n";
+            return;
+        }
 
+    cout << "\nThe Element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+    if (FRONT == REAR) {
+       FRONT = -1;
+       REAR = -1;
+    }
+    else {
+        if (FRONT == max -1)
+        FRONT = 0;
+        else 
+        FRONT + FRONT +1;
+    }   
+ }  
+
+  
 };
